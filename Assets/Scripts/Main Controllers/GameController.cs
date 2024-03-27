a5ff5151968e51ebd1b7b34d4eccaf2d9b68abce
iron-gabri97 <ferro197@hotmail.it>
2024-03-28 00:23:31 +0100

    I finish implementing the logic of the SceneNavigationController script and start implementing the code of the GameController script

--- Assets/Scripts/Main Controllers/GameController.cs
+++ Assets/Scripts/Main Controllers/GameController.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class GameController : MonoSingleton<GameController>
 {
+    //ENUMS
+    public enum EGameState
+    {
+        Start,
+        Playing,
+        Paused,
+        GameOver,
+        Quitting,
+        Exiting
+    }
+
+    //DATA
+    ///SIMPLE DATA
+    private EGameState state = 0;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Main Controllers/SceneNavigationController.cs
+++ Assets/Scripts/Main Controllers/SceneNavigationController.cs
@@ -5,20 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigationController : MonoBehaviour
 {
+    //ENUMS
     public enum eSceneName
     {
         MainMenu, 
         Raindrops
-    }    
+    }
+
+    //DATA
+
+    //SERIALIZED LIST OF ALL STAGES
+    [SerializeField] private List<StageDataSO> StageData = new();
+
+
+    //DICTIONARY FOR ACCESS TO THE RUNTIME
+    private Dictionary<eSceneName, StageDataSO> StageSceneDictionary = new();
+
+
+    //HAS LOADED
+    private bool hasLoaded = false;
+    public bool HasLoaded { get { return hasLoaded; } }
+
+    //METHODS 
+
+
     // Start is called before the first frame update
     void Start()
     {
+    
+        foreach (StageDataSO s in StageData)
+        {
+            StageSceneDictionary.Add(s.StageID, s);
+        }
         
+        hasLoaded = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    //FUNCTIONALITIES
+
+    public void LoadScene(eSceneName targetScene)
     {
-        
+        string intendedScene = StageSceneDictionary[targetScene].AssociatedSceneName;
+
+        if (string.IsNullOrEmpty(intendedScene))
+            SceneManager.LoadScene(intendedScene);
+        else
+            Debug.Log("Invalid Target Scene: " + targetScene);
     }
+
 }
