3e6fe6784cc3e3b2ccba0497048f30c858f96ec8
iron-gabri97 <ferro197@hotmail.it>
2024-03-31 18:55:12 +0200

    Updated Game Controller script logic

--- Assets/Scripts/Main Controllers/GameController.cs
+++ Assets/Scripts/Main Controllers/GameController.cs
@@ -16,18 +16,83 @@ public class GameController : MonoSingleton<GameController>
     }
 
     //DATA
-    ///SIMPLE DATA
+    //SIMPLE DATA
     private EGameState state = 0;
+    public bool IsPaused { get { return this.state == EGameState.Paused; } }
+    public bool isGameOver { get { return this.state == EGameState.GameOver; } }
+    public bool isPlaying { get { return !(IsPaused || isGameOver); } }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("GameController is starting!");
+
+        SetState(EGameState.Start);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    //FUNCTIONALITIES
+    public void SetState(EGameState targetState)
+    {
+        state = targetState;
+        switch (state) 
+        {
+            case EGameState.Start:
+                //RESERVED FOR INITIALIZATION
+
+                SetState(EGameState.Playing);
+                break;
+
+            case EGameState.Playing:
+
+                break;
+
+            case EGameState.Paused:
+
+                break;
+
+            case EGameState.GameOver:
+
+                break;
+
+            case EGameState.Quitting:
+
+                break;
+
+            case EGameState.Exiting:
+
+                break;
+        }
+    }
+
+
+    //QUIT GAME (ABANDON SESSION)
+    private static void QuitGame()
+    {
+        //TODO: GO BACK TO MAIN MENU
+        //NOW IT QUITS THE GAME
+        ExitGame();
+    }
+
+
+    //EXIT GAME
+    private static void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+#endif
     }
 }
