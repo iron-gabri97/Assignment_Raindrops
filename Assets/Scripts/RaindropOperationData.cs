9404baf7cfbf4e5c51910bb7d1d629e6ae4d6505
iron-gabri97 <ferro197@hotmail.it>
2024-03-31 20:53:45 +0200

    Update Game Controller prefab and Raindrops scene. Created RaindropOperationData script, entering some data.

---
+++ Assets/Scripts/RaindropOperationData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaindropOperationData : MonoBehaviour
+{
+
+    //ENUMS
+    public enum EOperation
+    {
+        Sum, 
+        Difference,
+        Multiplication,
+        Division
+    }
+
+    //DATA
+    private EOperation operation;
+    private int firstNumber;
+    private int secondNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
