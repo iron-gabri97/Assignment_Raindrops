0b0b49392d6e678dde07cd0f3851fbd7528093ab
iron-gabri97 <ferro197@hotmail.it>
2024-04-03 20:17:25 +0200

    Updated GameController script, adding restart status.

--- Assets/Scripts/Main Controllers/GameController.cs
+++ Assets/Scripts/Main Controllers/GameController.cs
@@ -11,6 +11,7 @@ public class GameController : MonoSingleton<GameController>
         Playing,
         Paused,
         GameOver,
+        Restarting,
         Quitting,
         Exiting
     }
@@ -21,6 +22,9 @@ public class GameController : MonoSingleton<GameController>
     public bool IsPaused { get { return this.state == EGameState.Paused; } }
     public bool isGameOver { get { return this.state == EGameState.GameOver; } }
     public bool isPlaying { get { return !(IsPaused || isGameOver); } }
+
+    //GAMEOBJECT
+    [SerializeField] SceneNavigationController sceneNavigationController;
 
 
 
@@ -62,7 +66,9 @@ public class GameController : MonoSingleton<GameController>
             case EGameState.GameOver:
 
                 break;
-
+            case EGameState.Restarting:
+                RestartGame();
+                break;
             case EGameState.Quitting:
 
                 break;
@@ -73,6 +79,8 @@ public class GameController : MonoSingleton<GameController>
         }
     }
 
+    //RESTART GAME
+    private static void RestartGame() => Instance.sceneNavigationController.LoadScene(SceneNavigationController.eSceneName.Raindrops);
 
     //QUIT GAME (ABANDON SESSION)
     private static void QuitGame()
